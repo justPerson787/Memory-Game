0349c0f6e19b2633efe4c84054f571a9c40e91aa
Natalia-Ushakova <n.n.ushakova@gmail.com>
2018-08-20 22:02:35 -0400

    First commit

--- README.md
+++ README.md
@@ -1,20 +1,12 @@
 # Memory Game Project
 
-## Table of Contents
+## About Project
 
-* [Instructions](#instructions)
-* [Contributing](#contributing)
+This project has been created as a part of Udacity's Front End Web Development nanodegree.
+The starter project had some HTML and CSS styling to display a static version of the Memory Game.
 
 ## Instructions
 
-The starter project has some HTML and CSS styling to display a static version of the Memory Game project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+For details look at the project instructions in the [Udacity Classroom](https://classroom.udacity.com/me).
 
-To get started, open `js/app.js` and start building out the app's functionality
 
-For specific, detailed instructions, look at the project instructions in the [Udacity Classroom](https://classroom.udacity.com/me).
-
-## Contributing
-
-This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
-
-For details, check out [CONTRIBUTING.md](CONTRIBUTING.md).
--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 /*
  * Create a list that holds all of your cards
  */
-
-
+let cards = ['fa-paper-plane-o','fa-diamond','fa-anchor','fa-bolt','fa-bomb','fa-leaf','fa-bicycle','fa-cube', 'fa-paper-plane-o','fa-diamond','fa-anchor','fa-bolt','fa-bomb','fa-leaf','fa-bicycle','fa-cube'];
+let openCards = [];
+let matchedCards = [];
+let moves = 0;
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
@@ -25,6 +27,21 @@ function shuffle(array) {
     return array;
 }
 
+//Initiating a game
+function new_deck() {
+    let ul = document.querySelectorAll(".deck i");
+    for (let n = 0; n < ul.length; n++) {
+        ul[n].classList.remove("fa");
+        ul[n].classList.remove(cards[n]);
+    }
+    shuffle(cards);
+    for (let n = 0; n < ul.length; n++) {
+        ul[n].classList.add("fa");
+        ul[n].classList.add(cards[n]);
+    }
+}
+
+new_deck();
 
 /*
  * set up the event listener for a card. If a card is clicked:
@@ -36,3 +53,61 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+ function moveCounter() {
+     moves += 1;
+     document.getElementById("moves").textContent = moves;
+
+ }
+
+ // function to open cards
+function open_card(c) {
+    c.classList.add("open");
+    c.classList.add("show");
+    openCards.push(c);
+}
+
+function matched() {
+    openCards[0].classList.add('match');
+    openCards[1].classList.add('match');
+    openCards = []; 
+    moveCounter();   
+}
+
+function unmatched() {
+    //openCards[1].style.backgroundColor = "pink";
+    setTimeout(function(){
+        openCards[0].classList.remove('open', 'show');
+        openCards[1].classList.remove('open', 'show');
+        openCards = [];
+    }, 700);
+    moveCounter();
+}
+
+function restart() {
+    matchedCards.forEach(function(element) {
+        element.classList.remove('open', 'show', 'match');
+    });
+    new_deck();
+    matchedCards = [];
+    // reset move counter
+    moves = 0;
+    document.getElementById("moves").textContent = moves;
+}
+
+document.querySelector('.deck').addEventListener('click', function (evt) {
+    if (event.target.nodeName === "LI"){
+        open_card(event.target)
+    };
+    if (openCards.length == 2 && openCards[0].innerHTML == openCards[1].innerHTML) {
+        matchedCards.push(openCards[0], openCards[1]);
+        matched()        
+    }; 
+    if (openCards.length == 2 && openCards[0].innerHTML != openCards[1].innerHTML) {
+        unmatched()
+    }
+
+    
+});
+
+
